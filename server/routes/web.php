02e07e68196d4adb045516fc86253cf675854f4d
Moritz Cornielje <90604542+mcornielje090404@users.noreply.github.com>
2023-05-30 22:18:24 +0100

    Inertia back-end init

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get(
+    '/',
+    function () {
+        return Inertia::render(
+            'app'
+        );
+    }
+);
